58821df7e897cf1976d5fb720a7d290fc2834175
DIR\s.vertedor.beltran <vertedor86@gmail.com>
2023-03-31 14:51:56 +0200

    Y massss

--- Probar/src/ejer1/ProbandoApp.java
+++ Probar/src/ejer1/ProbandoApp.java
@@ -5,6 +5,7 @@ import static ejer1.Probando.suma;
 
 public class ProbandoApp {
     public static void main(String[] args) {
+        
 
         System.out.println(suma(8 ,6));
         System.out.println(resta(7 ,1));
